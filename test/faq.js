0bdd12c83b76a7faeb1cc3bc175b7860d06f4b1b
Smasherly <ashholbrook@gmail.com>
2019-02-15 17:42:12 -0600

    pause section done

--- test/faq.js
+++ test/faq.js
@@ -1,10 +1,28 @@
 describe("Homepage FAQs Accordian", () => {
-    it("shoudl show first section on page load", () =>{
+    beforeEach  ("open home page", () => {
         browser.url("/");
+    });
+    it("should show first section on page load", () =>{
         let firstHeight = browser.getCssProperty(".accordion .accordion-item:first-child .accordion-content", "height");
-
-        console.log(firstHeight);
-
         expect(firstHeight.parsed.value).to.be.greaterThan(0)
-    } )
+        
+    })
+    it("should not show other content", () => {
+        let secondDisplay = browser.getCssProperty(".accordion .accordion-item:nth-of-type(2) .accordion-content", "display")
+        expect(secondDisplay.value).to.equal("none");
+    })
+    it("should expand/hide content on click", () => {
+        browser.click(".accordion .accordion-item:nth-of-type(2)")
+        browser.pause(1000);
+        let secondHeight = browser.getCssProperty(".accordion .accordion-item:nth-of-type(2) .accordion-content", "height")
+        console.log("secondHeight", secondHeight)
+        expect(secondHeight.parsed.value).to.be.greaterThan(0)
+
+        let firstDisplay = browser.getCssProperty(".accordion .accordion-item:first-child .accordion-content", "display")
+        expect(firstDisplay.value).to.equal("none");
+
+    })
 }) 
+
+
+
